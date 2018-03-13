26de92d82d92fc94b94556aecd07b83701c49d09
mengfei <m1220103093@163.com>
2018-03-13 17:42:13 +0800

    孟菲 修改孙吉的错事

---
+++ mf-juanpi/src/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+   	<router-link active-class= "active" to="/" exact>财经</router-link>
+   	<router-link active-class= "active" to="News">新闻</router-link>
+   	<router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.active{
+	color:red;
+}
+</style>
