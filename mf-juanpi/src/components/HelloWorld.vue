a07c036e09432c72cf2d1309dbf3be7a714e8c59
sunji <593357226@qq.com>
2018-03-14 18:26:24 +0800

    abc

--- mf-juanpi/src/components/HelloWorld.vue
+++ mf-juanpi/src/components/HelloWorld.vue
@@ -1,23 +1,15 @@
 <template>
   <div class="hello">
-<<<<<<< HEAD:mf-juanpi/src/components/HelloWorld.vue
    	
    	<!--<Search></Search>-->
-=======
   	<Choiceness></Choiceness>
->>>>>>> 9cdc4ca421940c10a7ee469334b135085f24d48a:mf-juanpi/src/components/HelloWorld.vue
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD:mf-juanpi/src/components/HelloWorld.vue
 //	import Search  from '@/components/index/search'
-
-	
-=======
 	import Choiceness from '@/components/index/choiceness'
 
->>>>>>> 9cdc4ca421940c10a7ee469334b135085f24d48a:mf-juanpi/src/components/HelloWorld.vue
 export default {
   name: 'HelloWorld',
   data () {
@@ -25,15 +17,12 @@ export default {
       msg: 'Welcome to Your Vue.js App'
     }
   },
-<<<<<<< HEAD:mf-juanpi/src/components/HelloWorld.vue
   components : {
 //	Search,
   }
-=======
   components:{
   	Choiceness
   },
->>>>>>> 9cdc4ca421940c10a7ee469334b135085f24d48a:mf-juanpi/src/components/HelloWorld.vue
 }
 </script>
 
